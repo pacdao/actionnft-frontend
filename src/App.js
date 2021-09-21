a26f22202882fbdfcb80c233ba706b6ea1a57dc8
Gerrit Hall <gerrit@rezscore.com>
2021-09-21 07:31:05 -0700

    App Page

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Main from "pages/main";
 import Header from "components/Header";
 import Footer from "components/Footer";
 import pacImageCommon from "assets/hendlinCommon.jpg";
-import { Grid, Grow, makeStyles } from "@material-ui/core";
+import { Grid, Grow, makeStyles, Typography } from "@material-ui/core";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -42,19 +42,31 @@ function App() {
 
             <Alert severity="error">
               Please install{" "}
-              <a href="https://metamask.io/" target="_blank" rel="noreferrer">
+              <a href="https://metamask.io/" target="_blank" rel="noreferrer" style={{color: '#000;'}}>
                 Metamask
               </a>{" "}
               first.
             </Alert>
 
 
+          <Typography display="block" style={{ margin: "1rem 0" }} gutterBottom>
+<br/>
+	<center>	
+	  <strong>'The War on Crypto'</strong> features award-winning American artist <strong><a style={{color: '#000'}} href='https://www.rebeccahendin.com/' target='_blank'>Rebecca Hendin</a></strong>, whose art is regularly featured in BBC, and The Guardian. This limited series reflects the current state of US politics and their overarching stranglehold on crypto innovation.
 
+		<br/><br/>
+		Connect A Web3 Provider Like MetaMask to Learn More
+		</center>
+		</Typography>
 
           </Grid>
         )}
         {provider && !isCorrectChain && (
+
+
           <Grid container justifyContent="center">
+
+
             <Alert severity="error">Please switch to Ethereum Mainnet.</Alert>
           </Grid>
         )}
