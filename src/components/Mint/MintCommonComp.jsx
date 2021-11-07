1ee9b0a5d256d4706eebee17b488e284ec60f5e3
Gerrit Hall <gerrit@rezscore.com>
2021-11-07 00:12:56 -0700

    feat: Refund button on site

--- src/components/Mint/MintCommonComp.jsx
+++ src/components/Mint/MintCommonComp.jsx
@@ -49,7 +49,7 @@ const MintCommonComp = () => {
 
   const { contract, commonPrice, quantity, totalSupply } = state;
 
-  const getCommonPrice = React.useCallback(
+  const getCommonPriceOld = React.useCallback(
     async (value = 1) => {
       if (value >= 1) {
         try {
@@ -64,9 +64,20 @@ const MintCommonComp = () => {
     [contract]
   );
 
+  const getCommonPrice = React.useCallback(async () => {
+    try {
+      const fetchedTotalSupply = (await contract.balanceOf(account)).toString();
+      return fetchedTotalSupply;
+    } catch (error) {
+      console.log("API ERROR totalSupply", error);
+      throw error;
+    }
+  }, [contract]);
+
+
   const getTotalMinted = React.useCallback(async () => {
     try {
-      const fetchedTotalSupply = (await contract.totalSupply()).toString();
+      const fetchedTotalSupply = (await contract.originalMintCount(account)).toString();
       return fetchedTotalSupply;
     } catch (error) {
       console.log("API ERROR totalSupply", error);
@@ -78,10 +89,8 @@ const MintCommonComp = () => {
     evt.preventDefault();
     try {
       dispatch({ type: TYPE.pending, message: "Please be patient, this will take a bit of time." });
-      const eth = parseUnits(commonPrice, "ether");
-      const wei = formatUnits(eth, "wei");
       const contract = new ethers.Contract(address, state.abi, signer);
-      const txResp = await contract.mintMany(quantity, { value: wei.toString() });
+      const txResp = await contract.refundAll();
       const { blockHash } = await txResp.wait();
       const [fetchedCommonPrice, fetchedTotalSupply] = await Promise.all([getCommonPrice(), getTotalMinted()]);
       dispatchSuccess({ commonPrice: fetchedCommonPrice, totalSupply: fetchedTotalSupply, blockHash });
@@ -136,69 +145,62 @@ const MintCommonComp = () => {
           <img className={classes.img} alt="PAC Crypto Activism NFT" src={pacImageCommon} />
         </Grow>
       </Grid>
+
+      <Grid item container xs={12} justifyContent="center">
+        <Grid item xs={12} container direction="column" justifyContent="center" alignItems="center">
+          <Typography display="block" style={{ margin: "1rem 0", textAlign: "center" }} gutterBottom>
+            Final Mint Price: <b>0.0792 ETH</b>
+            <br />
+            Total minted <b>292</b>
+	    <br/><br/>
+	    <em><strong>Available For 90% Refund</strong>
+            <br/>The infrastructure bill passed and no Congressperson introduced an amendment to remove the crypto language</em>.
+            <br/>If you still own the same quantity (or more) of original pieces, you can burn them for a 90% refund until December 6.
+
+	  <hr/>
+	  <strong>Refund Eligibility</strong>
+          </Typography>
+        </Grid>
+      </Grid>
+
       <Grid item xs={12} container justifyContent="center">
+
+
+            Your Total Mint Quantity: {totalSupply}
+	  <br/>
+	  Current Balance: {commonPrice}
+	  <br/>
+
+	<br/>
+
+
+	{ totalSupply == 0 ? "❌ You Never Minted" : "✅ Total Mint Quantity >0"}<br/>
+	{ commonPrice == 0 ? "❌ You Have No Balance" : "✅ Current Balance >0"}<br/>
+	{ totalSupply == 0 || commonPrice == 0 || commonPrice < totalSupply ? "❌ Insufficient Balance" : "✅ Balance Matches or Exceeds Mint"}
+         
         <Alerts status={state.status} blockHash={state.blockHash} errorMessage={state.errorMessage} />
         <Grid item xs={12} container justifyContent="center" alignItems="stretch">
-          <TextField
-            label="Your ETH amount"
-            variant="outlined"
-            style={{ margin: 8 }}
-            onChange={() => {}}
-            disabled
-            value={commonPrice}
-          />
-          <TextField
-            className={classes.textfield}
-            label="Quantity"
-            variant="outlined"
-            style={{ margin: "8px 0" }}
-            type="number"
-            min="1"
-            onChange={async ({ target: { value } }) => {
-              try {
-                dispatchSuccess({ quantity: value });
-                const qty = Number(value);
-                if (qty && qty >= 1) {
-                  const newMinPrice = await getCommonPrice(value);
-                  dispatchSuccess({ commonPrice: newMinPrice });
-                } else {
-                  dispatchSuccess({ commonPrice: "0" });
-                }
-              } catch (error) {
-                dispatchError(error.message);
-                throw error;
-              }
-            }}
-            disabled={!account}
-            value={quantity}
-          />
-
           <ProgressBtn
             className={classes.mintBtn}
             style={{ margin: "8px 0" }}
             variant="contained"
             color="primary"
             size="large"
-            loading={state.status === TYPE.pending}
             handleClick={handleMint}
-            disabled="true"
             type="submit"
+	    disabled={ totalSupply == 0 || commonPrice == 0 || commonPrice < totalSupply }
           >
-            Sale Ended
+	    { totalSupply == 0 || commonPrice == 0 || commonPrice < totalSupply ? "Ineligible" : "Refund All"}
           </ProgressBtn>
         </Grid>
       </Grid>
       <Grid item container xs={12} justifyContent="center">
-        <Grid item xs={12} container direction="column" justifyContent="center" alignItems="center">
-          <Typography display="block" style={{ margin: "1rem 0", textAlign: "center" }} gutterBottom>
-            Last Mint Price: <b>{commonPrice} ETH</b>
-            <br />
-            Total minted <b>{totalSupply}</b>
-	    <br/><br/>
-	    <em><strong>Eligible for 90% Refund</strong>
-            <br/>The infrastructure bill passed and no Congressperson introduced an amendment to remove the crypto language</em>.
-            <br/>If you still own the same quantity (or more) of original pieces, you can burn them for a 90% refund until December 6.<br/><br/>
-          <strong>Instructions</strong>
+        <Grid item md={8} xs={12}>
+	  <hr/>
+<center>
+          <strong>Alternate Instructions</strong>
+	  <br/>
+	  <em>If this button doesn't work, you can try manually on Etherscan</em>
           <br/>
             1. Visit the write contract section <a href='https://etherscan.io/address/0xe60a7825a80509de847ffe30ce2936dfc770db6b#writeContract'>on Etherscan</a><br/>
             2. Connect your wallet<br/>
@@ -214,19 +216,15 @@ const MintCommonComp = () => {
                 <br/>
   If you plan to claim your <a href='https://github.com/pacdao/bonus-nft' target='_blank'>free bonus NFT</a>, make sure to do so before burning.
 
-          <br/>
-          <br/>
+
+<br/><br/>
                 <hr/>
 
 <br/><br/>
 	  Published at <a href='https://etherscan.io/address/0xe60a7825a80509de847ffe30ce2936dfc770db6b' target='_blank'
 	  >0xe60a7825a80509de847ffe30ce2936dfc770db6b</a>
+</center>
 
-          </Typography>
-        </Grid>
-      </Grid>
-      <Grid item container xs={12} justifyContent="center">
-        <Grid item md={8} xs={12}>
           <RebeccaHendin />
           <Typography display="block" style={{ margin: "1rem 0" }} gutterBottom>
             If we succeed, then we've come from nowhere to save cryptocurrency from bureaucratic meddling. If we fail,
